ec966320c5b478f313528eba2188441786897d21
programacaogabriel <programacaogabriel@gmail.com>
2023-02-17 10:06:28 -0300

    :hammer: Botões
    Adicionado funções em JS para realizar a troca de cores

---
+++ script.js
@@ -0,0 +1,155 @@
+let button = document.getElementById("button");
+let dark = true
+let colors = ["white", "black", "red", "blue", "yellow", "violet", "green", "orange", "#953553", "#FF4433", "#9ACD32", "#FFAE42", "#293462", "#40e0d0"]
+
+
+// Cores Primarias
+function escolha_background() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "yellow"
+    }
+}
+
+function escolha_background1() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "red"
+    }
+}
+
+
+function escolha_background2() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "blue"
+    }
+}
+
+/// Cores Secundarias 
+
+function escolha_background3() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "violet"
+    }
+}
+
+function escolha_background4() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "green"
+    }
+}
+
+function escolha_background5() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "orange"
+    }
+}
+
+//Cores Terciárias
+
+function escolha_background6() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#953553"
+    }
+}
+
+function escolha_background7() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#FF4433"
+    }
+}
+
+
+
+function escolha_background8() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#9ACD32"
+    }
+}
+
+
+
+function escolha_background9() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#FFAE42"
+    }
+}
+
+
+function escolha_background10() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#293462"
+    }
+} id = "bt0"
+
+function escolha_background11() {
+
+    if (colors == false) {
+        colors = true
+        document.body.style.background = colors
+    } else {
+        dark = false
+        document.body.style.background = "#40e0d0"
+    }
+}
+
+
+
+
+
+
+
+
